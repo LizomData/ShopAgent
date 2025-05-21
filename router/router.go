0f5baa5c9b641f874f80511cca711ebb78d17c59
hgy <m17878660695@163.com>
2025-05-22 00:52:18 +0800

    Merge remote-tracking branch 'origin/main'

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"ShopAgent/controller/commodityCategoryController"
 	"ShopAgent/controller/commodityController"
+	"ShopAgent/controller/imageUploadController"
 	"ShopAgent/controller/purchase_controller"
 	"ShopAgent/controller/salespersonController"
 	"ShopAgent/controller/supplierController"
@@ -70,6 +71,15 @@ func Routers() *gin.Engine {
 		group.POST("/query", salespersonController.Query)
 		group.POST("/update", salespersonController.Update)
 		group.POST("/delete", salespersonController.Delete)
+	}
+
+	// 暴露静态文件目录
+	r.Static(imageUploadController.Uploader.Config.PublicPath, imageUploadController.Uploader.Config.UploadDir)
+	group = r.Group("/api/v1/image")
+	group.Use(util.AuthMiddleware())
+	{
+		group.POST("/upload", imageUploadController.Upload)
+		group.POST("/query", imageUploadController.Query)
 	}
 
 	return r
